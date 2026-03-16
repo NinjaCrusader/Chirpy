baf5c01d42326b1d68e73e3a801a83f344c7de6d
NinjaCrusader <jeroldpemberton@gmail.com>
2026-03-16 19:57:01 -0400

    added a basic index.html file and rebuilt the server

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	s := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatalf("there was an error with the server: %v\n", err)
