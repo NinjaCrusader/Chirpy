6d554d8e443f5093545828175dea08dd205f4bec
NinjaCrusader <jeroldpemberton@gmail.com>
2026-03-21 21:59:25 -0400

    moved the metrics and reset to admin paths. also changed how metrics worked and it now serves an admin page.

--- metrics.go
+++ metrics.go
@@ -8,9 +8,12 @@ import (
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 
-	count := fmt.Sprintf("Hits: %v\n", cfg.fileserverHits.Load())
+	count := cfg.fileserverHits.Load()
 
-	_, err := w.Write([]byte(count))
+	adminMetrics := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>\n", count)
+
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	_, err := w.Write([]byte(adminMetrics))
 	if err != nil {
 		log.Printf("there was an error writing the hit count: %v\n", err)
 	}
